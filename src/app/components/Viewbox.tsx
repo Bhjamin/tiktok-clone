77fcf35faf39f06de8c0ecbd134c4c20cf9c9809
Ben Hancock <benjamin@hancock1.com>
2024-04-30 19:24:14 -0600

    nice

--- src/app/components/Viewbox.tsx
+++ src/app/components/Viewbox.tsx
@@ -2,9 +2,22 @@
 
 import { useState, useEffect } from "react";
 import Video from "./Video";
+import axios from "axios";
 
 const Viewbox = () => {
   const [videos, setVideos] = useState([]);
+
+  const key = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    console.log(key);
+  }, [key]);
+
+  console.log(key);
+
+  // const fetchData = async () => {
+  //   await axios.get(`https://api.thecatapi.com/v1/images/search&api_key=${}`)
+  // }
 
   return (
     <div className="w-full h-full flex justify-center items-center">
