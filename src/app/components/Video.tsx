a95fa5ed597f66dbd2b5c321c925f1ecaf2b593d
Ben Hancock <benjamin@hancock1.com>
2024-09-26 16:55:48 -0600

    oh yeah baby

--- src/app/components/Video.tsx
+++ src/app/components/Video.tsx
@@ -1,8 +1,40 @@
 "use client";
+import { useInView } from "framer-motion";
+import { useRef, useEffect } from "react";
 
-const Video = () => {
+const Video = ({
+  video,
+  fetchData,
+  index,
+  length,
+}: {
+  video: string;
+  fetchData?: () => void;
+  index: number;
+  length: number;
+}) => {
+  const ref = useRef<HTMLImageElement>(null);
+  const isInView = useInView(ref, { margin: "-300px 0px -300px" });
+
+  useEffect(() => {
+    if (isInView) {
+      setTimeout(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth", block: "center" });
+      }, 300);
+    }
+    if (index === length - 2 && isInView) {
+      // fetchData();
+      console.log("fetching data");
+    }
+  }, [isInView]);
+
   return (
-    <div className="w-full h-full shrink-0 max-w-[425px] min-w-[300px] min-h-[600px] max-h-[825px] bg-neutral-700 flex justify-center items-center rounded-3xl"></div>
+    <div className="w-full h-full shrink-0 max-w-[425px] min-w-[300px] min-h-[600px] max-h-[825px] bg-neutral-700 flex justify-center items-center rounded-3xl relative">
+      <span className="absolute bottom-2 left-2 w-8 h-8 flex items-center justify-center bg-black text-white rounded-full">
+        {index + 1}
+      </span>
+      <img ref={ref} src={video} />
+    </div>
   );
 };
 
--- src/app/components/Viewbox.tsx
+++ src/app/components/Viewbox.tsx
@@ -5,26 +5,47 @@ import Video from "./Video";
 import axios from "axios";
 
 const Viewbox = () => {
-  const [videos, setVideos] = useState<Array<string>>([]);
+  const [videos, setVideos] = useState<Array<string>>([
+    "https://cdn2.thecatapi.com/images/3ic.jpg",
+    "https://cdn2.thecatapi.com/images/3v4.jpg",
+    "https://cdn2.thecatapi.com/images/4h4.gif",
+    "https://cdn2.thecatapi.com/images/bcu.jpg",
+    "https://cdn2.thecatapi.com/images/c18.jpg",
+    "https://cdn2.thecatapi.com/images/d0g.png",
+    "https://cdn2.thecatapi.com/images/dlo.jpg",
+    "https://cdn2.thecatapi.com/images/MjA0MDUxMA.jpg",
+    "https://cdn2.thecatapi.com/images/MjA2MTgzMw.jpg",
+    "https://cdn2.thecatapi.com/images/aU69p2mTT.jpg",
+  ]);
 
   const key = process.env.NEXT_PUBLIC_API_KEY;
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  const fetchData = async () => {
-    const response = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?limit=3&api_key=${key}`
-    );
-    const urls = response.data.map((pic) => pic.url);
-    setVideos([...videos, ...urls]);
-  };
+  // console.log(videos);
+
+  // const fetchData = async () => {
+  //   const response = await axios.get(
+  //     `https://api.thecatapi.com/v1/images/search?limit=3&api_key=${key}`
+  //   );
+  //   const urls = response.data.map((pic) => pic.url);
+  //   setVideos([...videos, ...urls]);
+  // };
 
   return (
     <div className="w-full flex flex-col justify-start items-center py-5 gap-5">
-      {videos.map((video) => {
-        return <Video />;
+      {videos.map((video, i) => {
+        return (
+          <Video
+            key={video}
+            video={video}
+            // fetchData={fetchData}
+            index={i}
+            length={videos.length}
+          />
+        );
       })}
     </div>
   );
